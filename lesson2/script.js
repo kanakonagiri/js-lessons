35bdbfe5f357d37fce00c8c06af85327cf21274b
kanakonagiri <kanako22n@gmail.com>
2021-08-20 23:19:31 +0900

    pref:coding changes

--- lesson2/script.js
+++ lesson2/script.js
@@ -1,27 +1,17 @@
     const li = document.createElement('li');
 
     const a = document.createElement('a');
-    const href = document.createAttribute('href');
-    href.value = "1.html";
-    a.setAttributeNode(href);
+    a.href = "1.html";
 
     const img = document.createElement('img');
-    const src = document.createAttribute('src');
-    src.value = "bookmark.png";
-    img.setAttributeNode(src);
-    const alt = document.createAttribute('alt');
-    alt.value = "ブックマーク";
-    img.setAttributeNode(alt);
+    img.src = "bookmark.png";
+    img.alt = "ブックマーク";
 
     const text = document.createTextNode('これです');
 
-    const frag = new DocumentFragment();
     a.appendChild(img);
     a.appendChild(text);
     li.appendChild(a);
-    frag.appendChild(li);
-
-    //ulに挿入
-    document.getElementById('js-lists').appendChild(frag);
+    document.getElementById('js-lists').appendChild(li);
 
 
