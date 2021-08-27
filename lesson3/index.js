77c6ef520dcb93dc43b1b6159937ac22e406aebe
kanakonagiri <kanako22n@gmail.com>
2021-08-27 12:47:06 +0900

    fix:change variable name

--- lesson3/index.js
+++ lesson3/index.js
@@ -1,17 +1,17 @@
-const list = [
+const listData = [
     {href:'a1.html', src:'img/bookmark.png', text:'a1'},
     {href:'a2.html', src:'img/message.png', text:'a2'}
 ]
 
 const lists = document.getElementById('js-lists');
 
-list.forEach((listData) => {
+listData.forEach((list) => {
     const li = document.createElement('li');
     const a = document.createElement('a');
-    a.href = listData.href;
+    a.href = list.href;
     const img = document.createElement('img');
-    img.src = listData.src;
-    const text = document.createTextNode(listData.text);
+    img.src = list.src;
+    const text = document.createTextNode(list.text);
 
     a.appendChild(img);
     a.appendChild(text);
