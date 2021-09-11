082269d2818f8c85762fe273f9d8def84230ada8
kanakonagiri <kanako22n@gmail.com>
2021-09-11 21:54:09 +0900

    fix：fixed the usage of promise objects

--- lesson5/index.js
+++ lesson5/index.js
@@ -1,33 +1,25 @@
-Promise.all([
-    {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"}, 
+const listData = [
+    {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
     {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
-]).then(
-    response => {
-        const frag = new DocumentFragment();
+]
+const myPromise = new Promise((resolve) => {
+    resolve(listData);
+});
+myPromise.then(() => {
+    const frag = new DocumentFragment();
+    for(const list of listData) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = list.to;
+        const img = document.createElement('img');
+        img.src = list.img;
+        img.alt = list.alt;
+        const text = document.createTextNode(list.text);
 
-        for(const list of response) {
-            const li = document.createElement('li');
-            const a = document.createElement('a');
-            a.href = list.to;
-            const img = document.createElement('img');
-            img.src = list.img;
-            img.alt = list.alt;
-            const text = document.createTextNode(list.text);
-    
-            a.appendChild(img);
-            a.appendChild(text);
-            li.appendChild(a);
-            frag.appendChild(li);
-        }
-    
-        document.getElementById('js-lists').appendChild(frag);
-        
-    },
-    error => {
-        console.log('エラー');
+        a.appendChild(img);
+        a.appendChild(text);
+        li.appendChild(a);
+        frag.appendChild(li);
     }
-)
-
-
-
-
+    document.getElementById('js-lists').appendChild(frag);
+});
